8ce8f786748ea0eb5ce2cdd295ebd4e75295d788
Luis Redda <luis.redda@gmail.com>
2020-03-23 20:06:29 -0300

    Update ci-reports-ok.py

--- ci-reports-ok.py
+++ ci-reports-ok.py
@@ -41,7 +41,7 @@ class UntitledTestCase(unittest.TestCase):
             print ('application ok')  
             driver.find_element_by_id("application").click()
             print ('looking for api-monitor')
-            myElem = WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.ID, 'api-monitor')))
+            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'api-monitor')))
             print ('api-monitor ok')   
             Select(driver.find_element_by_id("application")).select_by_visible_text("api-monitor")
             #print ('looking for table')
