7376de7e58c3d63ef9e0d2134ea4e71bf4ebcef8
Luis Redda <luis.redda@gmail.com>
2020-03-23 15:38:05 -0300

    Update general-checker.py

--- general-checker.py
+++ general-checker.py
@@ -6,4 +6,4 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.google.com')
-#print(driver.title)
+print(driver.title)
